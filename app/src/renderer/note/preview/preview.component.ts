97131912be3cd4fdf7cb5ba57e0b5a82b88c6061
seokju-na <seokju.me@gmail.com>
2017-12-19 02:17:48 +0900

    Add note text preview markdown renderer
    Took 1 hour 24 minutes

--- app/src/renderer/note/preview/preview.component.ts
+++ app/src/renderer/note/preview/preview.component.ts
@@ -1,6 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { NoteBody } from '../models';
+import * as MarkdownIt from 'markdown-it';
+const hljs = require('highlight.js');
 
+
+const md = new MarkdownIt({
+    highlight(str, lang): string {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return '<pre class="hljs"><code>' +
+                    hljs.highlight(lang, str, true).value +
+                    '</code></pre>';
+            } catch (__) {}
+        }
+
+        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
+});
 
 @Component({
     selector: 'note-preview',
@@ -9,4 +26,11 @@ import { NoteBody } from '../models';
 })
 export class NotePreviewComponent {
     @Input() noteBody: NoteBody;
+
+    constructor(private sanitizer: DomSanitizer) {
+    }
+
+    parseMarkdown(value: string): SafeHtml {
+        return this.sanitizer.bypassSecurityTrustHtml(md.render(value));
+    }
 }
